6b6fc52667f0005c8e76e4c8be06fa6491dbffdc
joaocalix <joaopaulopilotto@gmail.com>
2023-10-31 22:46:21 +0000

    comandos importantes para usar github

---
+++ comandos_git.py
@@ -0,0 +1,10 @@
+# git add <arquivo> adc o arquivo
+
+# git status: informa o status dos arquivos do projeto
+
+# git commit: "Salvar" as novas edições
+
+# git push: Atualiza a pasta do projeto no GitHub
+
+# git restore --staged <arquivo>: desfaz ação caso um
+# arquivo seja comitado por engano
